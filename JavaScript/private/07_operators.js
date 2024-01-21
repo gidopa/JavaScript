be9e3ee672dfca4d4d1c8275febb1de4b1b36e18
Gido <jdm2131@naver.com>
2024-01-21 21:24:16 +0900

    js 공부

--- JavaScript/private/07_operators.js
+++ JavaScript/private/07_operators.js
@@ -95,4 +95,17 @@ console.log(10>0 ? '10이 0보다 크다' : '10이 0보다 작다')
  * && ||
  */
 console.log(true && false)
+// 지수 연산자 **
+console.log(2 ** 2)
+let name;
+console.log(name);
+// ?? -> l-value가 null이거나 undeifned 일때 ?? 우측의 값을 반환해라 
+name = name ?? '코드팩토리';
+console.log(name);
+name = name ?? '아이브';
+console.log(name) // 이미 코드팩토리 값이 저장이 되었음
+let name2;
+name2 ??= `코드팩토리`;
+console.log(name2)
+
 
