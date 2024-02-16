7d3ae1c190b97fa5df586630c41f0d250edf45f3
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2024-02-16 15:49:09 +0900

    jquery

---
+++ JavaScript/edu/2자바스크립트기초문법/JS/growth_c.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 5
+console.log('a++',a++)
+console.log('a',a)
+console.log('++a',++a)
+
+console.log('a+b',a+b)
+console.log('a+(++b)',a+(++b))
+console.log('a+(b++)',a+(b++))
+console.log('a+b',a+b)
---
+++ JavaScript/edu/2자바스크립트기초문법/JS/while_2_test.js
@@ -0,0 +1,10 @@
+var i = 1;
+while (true) {
+  if (i % 6 == 0) {
+    document.write(i + "<br>");
+  }
+  if (i > 30) {
+    break;
+  }
+  i++;
+}
